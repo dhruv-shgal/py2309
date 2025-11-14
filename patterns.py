be9d46844eeb055c33886e728c7f3bdeef58fb38
Dhruv sehgal <dhruvsehgal.022@gmail.com>
2025-11-14 19:44:13 +0530

    Initial commit

--- patterns.py
+++ patterns.py
@@ -45,11 +45,18 @@ n = int(input())
 #         flag=1-flag
 #     print("")               
 
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end="")
-    for j in range(2*(n-i)):
-        print(" ",end="")
-    for j in range(i):
-        print(i-j,end="")
-    print("")
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     for j in range(2*(n-i)):
+#         print(" ",end="")
+#     for j in range(i):
+#         print(i-j,end="")
+#     print("")
+
+# count =1
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(count,end=" ")
+#         count+=1
+#     print("")
