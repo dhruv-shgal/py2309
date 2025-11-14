87e0968d7f3844771a11a91acb4b3e61a066150d
Dhruv sehgal <dhruvsehgal.022@gmail.com>
2025-11-14 19:07:29 +0530

    first commit

---
+++ patterns.py
@@ -0,0 +1,38 @@
+n = int(input())
+# for i in range(1,n+1):
+#         for j in range(1,i+1):
+#             print(j,end="")
+#         print("")    
+
+
+# for i in range(1,n+1):
+
+    # for j in range(n-i):
+    #     print(end=" ")
+    # for j in range(2*i-1):
+    #     print("*",end="")    
+    # for j in range(n-i):
+    #     print(end=" ")
+    # print("")        
+    # for j in range(i):
+    #     print(end=" ")
+    # for j in range(2*(n-i)+1):
+    #     print("*",end="")
+    # for j in range(i):
+    #     print(end=" ")    
+    # print("")    
+
+
+# for i in range(1,2*n+1):
+#     if i > (2*n)//2:
+#         for j in range(2*n - i):
+#             print("*",end="")
+#         print(""    )
+#     else:     
+#         for j in range(i):
+#             print("*",end="")
+#         print("")
+       
+
+       
+
