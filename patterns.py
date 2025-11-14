b395e3728df1d741afac08c6cf74bd541445611f
Dhruv sehgal <dhruvsehgal.022@gmail.com>
2025-11-14 19:25:11 +0530

    Initial commit

--- patterns.py
+++ patterns.py
@@ -34,5 +34,13 @@ n = int(input())
 #         print("")
        
 
-       
 
+for i in range(1,n+1):
+    if i % 2==0:
+        flag=0
+    else:
+        flag=1
+    for j in range(i):
+        print(flag,end="")
+        flag=1-flag
+    print("")               
