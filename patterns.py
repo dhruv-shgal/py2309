cf687869fcc977c675aecbe8dc9f84dc38cc3a87
Dhruv sehgal <dhruvsehgal.022@gmail.com>
2025-11-14 19:50:33 +0530

    Initial commit

--- patterns.py
+++ patterns.py
@@ -60,3 +60,8 @@ n = int(input())
 #         print(count,end=" ")
 #         count+=1
 #     print("")
+
+for i in range(1,n+1):
+    for ch in range(ord('A'), ord('A')+i): #ord() function gives ASCII value of character
+        print(chr(ch),end="")
+    print("")    
